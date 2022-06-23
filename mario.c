9b5d8de7b567d48c2bb046a72f05b7498a737a38
Dangoness <sarah.lopes.silva01@gmail.com>
2022-06-23 11:39:22 -0300

    primeiro commit

---
+++ mario.c
@@ -0,0 +1,31 @@
+#include <cs50.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+          int altura;
+        
+     do
+     {
+          altura = get_int ("Altura: ");
+     }
+     while (altura < 1 || altura > 8);
+     
+
+     
+     // Imprima a #
+     
+    for(int i = 1; i <= altura; i++)
+    {
+	    for(int j = altura -i; j >= 1; j--)
+	    printf ( " " );
+	    
+	    for (int j = 1; j < i ; j++)
+    	{
+         		printf("#"); 
+    	}
+    	printf("\n"); 
+    }
+          
+     }
